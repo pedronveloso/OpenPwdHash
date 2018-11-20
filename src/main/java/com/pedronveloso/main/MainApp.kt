0da5755c772dc82b7dbb2f6ba1a392e90a7deb30
Pedro Veloso <velop@amazon.com>
2018-11-20 00:20:04 -0500

    First commit.
    Project setup with TornadoFX - running simple view. Implementation of Stanford's PwdHash algorithm.

---
+++ src/main/java/com/pedronveloso/main/MainApp.kt
@@ -0,0 +1,9 @@
+package com.pedronveloso.main
+
+import com.pedronveloso.views.GeneratorView
+import tornadofx.App
+
+/**
+ * References to available Views
+ */
+class MainApp: App(GeneratorView::class)
---
+++ src/main/java/com/pedronveloso/utils/Logger.kt
@@ -0,0 +1,10 @@
+package com.pedronveloso.utils
+
+
+fun logError(text: String) {
+    System.err.println(text)
+}
+
+fun print(text: String) {
+    System.out.println(text)
+}
